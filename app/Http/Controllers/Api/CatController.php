dc08060776df3a6103d99d26f9db7ac8db747736
Krzysztof <k.konwinski@onet.pl>
2023-06-13 11:27:26 +0200

    refactor controllers

--- app/Http/Controllers/Api/CatController.php
+++ app/Http/Controllers/Api/CatController.php
@@ -7,34 +7,34 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CatRequest;
 use App\Http\Resources\CatCollection;
 use App\Http\Resources\CatResource;
+use Illuminate\Http\Response;
 
 class CatController extends Controller
 {
-    public function index()
-{
-    return new CatCollection(Cat::paginate());
-}
+    public function index(): CatCollection
+    {
+        return new CatCollection(Cat::paginate());
+    }
 
-public function store(CatRequest $request)
-{
-    return new CatResource(Cat::create($request->validated()));
-}
+    public function store(CatRequest $request): CatResource
+    {
+        return new CatResource(Cat::create($request->validated()));
+    }
 
-public function show(Cat $cat)
-{
-    return new CatResource($cat);
-}
+    public function show(Cat $cat): CatResource
+    {
+        return new CatResource($cat);
+    }
 
-public function update(CatRequest $request, Cat $cat)
-{
-    $cat->update($request->validated());
-    return new CatResource($cat);
-}
+    public function update(CatRequest $request, Cat $cat): CatResource
+    {
+        $cat->update($request->validated());
+        return new CatResource($cat);
+    }
 
-public function destroy(Cat $cat)
-{
-    $cat->delete();
-    return response()->json(null, 204);
-}
-
+    public function destroy(Cat $cat): Response
+    {
+        $cat->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
 }
--- app/Http/Controllers/Api/WorkerController.php
+++ app/Http/Controllers/Api/WorkerController.php
@@ -7,33 +7,34 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\WorkerRequest;
 use App\Http\Resources\WorkerCollection;
 use App\Http\Resources\WorkerResource;
+use Illuminate\Http\Response;
 
 class WorkerController extends Controller
 {
-    public function index()
+    public function index(): WorkerCollection
     {
         return new WorkerCollection(Worker::paginate());
     }
 
-    public function store(WorkerRequest $request)
+    public function store(WorkerRequest $request): WorkerResource
     {
         return new WorkerResource(Worker::create($request->validated()));
     }
-    
-    public function show(Worker $worker)
+
+    public function show(Worker $worker): WorkerResource
     {
         return new WorkerResource($worker);
     }
-    
-    public function update(WorkerRequest $request, Worker $worker)
+
+    public function update(WorkerRequest $request, Worker $worker): WorkerResource
     {
         $worker->update($request->validated());
         return new WorkerResource($worker);
     }
-    
-    public function destroy(Worker $worker)
+
+    public function destroy(Worker $worker): Response
     {
         $worker->delete();
-        return response()->json(null, 204);
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
